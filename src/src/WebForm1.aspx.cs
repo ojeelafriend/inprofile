4d170a102c433f4baf2b07f05724727ad70c39ef
ElMargaritox <alexisguzman0303@gmail.com>
2021-04-17 01:21:25 -0300

    Receive Json To Auth

---
+++ src/src/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Usuarios;
+
+namespace src
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string Login(Usuario data)
+        {
+            string respuesta = "";
+            Usuario user = data;
+            respuesta = user != null ? "Bien" : "Mal";
+            return respuesta;
+        }
+    }
+}
